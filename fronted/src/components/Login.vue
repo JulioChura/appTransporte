848c6bf54ca1738ef3d1d8249c017354a896cf98
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-18 12:29:08 -0500

    Se agrega un modal para el login, se usa boostrap

--- fronted/src/components/Login.vue
+++ fronted/src/components/Login.vue
@@ -8,16 +8,64 @@ defineProps({
 </script>
 
 <template>
-  <b-modal v-model="showModal" title="Iniciar sesión">
-    <form @submit.prevent="login">
-      <input type="email" v-model="email" placeholder="Correo electrónico" required>
-      <input type="password" v-model="password" placeholder="Contraseña" required>
-      <button type="submit">Iniciar sesión</button>
-    </form>
-  </b-modal>
+  <div class="modal" v-if="isVisible">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2>Login</h2>
+      
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+    </div>
+  </div>
 </template>
 
 
 <style scoped>
-
+  .modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .modal-content {
+    background-color: #fefefe;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    max-width: 500px;
+  }
+  .close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+  }
+  .close:hover,
+  .close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
 </style>
