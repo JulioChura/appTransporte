ff0d59901e4ca73287e3d88fc45b13a9e7aac23f
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-18 12:10:54 -0500

    Se agrega el component  login y se crea la view de Home

---
+++ fronted/src/components/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import Cabecera from "../Cabecera.vue"
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <b-modal v-model="showModal" title="Iniciar sesión">
+    <form @submit.prevent="login">
+      <input type="email" v-model="email" placeholder="Correo electrónico" required>
+      <input type="password" v-model="password" placeholder="Contraseña" required>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  </b-modal>
+</template>
+
+<style scoped>
+
+</style>
