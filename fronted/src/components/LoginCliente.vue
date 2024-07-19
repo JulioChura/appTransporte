c41ae5c14d474a364fa09adce4ef2e275b6283af
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-18 20:47:47 -0500

    Se crea el metodo para enviar la data del logeo  al backend

--- fronted/src/components/LoginCliente.vue
+++ fronted/src/components/LoginCliente.vue
@@ -1,5 +1,22 @@
 <script setup>
 import { defineProps, defineEmits } from 'vue';
+import axios from 'axios';
+
+const email = ref("")
+const password = ref("");
+
+const login = async () => {
+  try {
+    const response = await axios.post("http://localhost:8000", {
+      email: email.value,
+      password: password.value
+    });
+    // recibir y almacenar el token (falta implementarlo)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const props = defineProps({
   isVisible: {
@@ -21,15 +38,15 @@ const closeModal = () => {
       <span class="close" @click="closeModal">&times;</span>
       <h2>Login</h2>
       
-      <form>
+      <form @submit.prevent="login">
         <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
--- fronted/src/main.js
+++ fronted/src/main.js
@@ -5,7 +5,12 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App);
+
+app.use(VueAxios, axios)
 
 app.use(router);
 
