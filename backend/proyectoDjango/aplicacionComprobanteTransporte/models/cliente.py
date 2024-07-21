520e4496b1cd6e06e79f945e94a63e0d68daa904
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-21 17:07:28 -0500

    Se corrige la verificacion del password en el modelo de cliente

--- backend/proyectoDjango/aplicacionComprobanteTransporte/models/cliente.py
+++ backend/proyectoDjango/aplicacionComprobanteTransporte/models/cliente.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 from .empresa import Empresa
+
 class Cliente(models.Model):
     id = models.BigAutoField(primary_key=True)
     Name = models.CharField(max_length=100)
@@ -11,14 +12,16 @@ class Cliente(models.Model):
     id_company = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='clientes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    email = models.EmailField(max_length=255, unique=True) 
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=128)
     
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-    
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-    
-    def __str__(self): 
+    def __str__(self):
         return f"{self.Name} {self.LastName}"
+    
+    def save(self, *args, **kwargs):
+        if self.pk is None:  # Solo para nuevos clientes
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def verify_password(self, password):
+        return check_password(password, self.password)
