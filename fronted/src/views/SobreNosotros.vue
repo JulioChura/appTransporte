35d33f39af0d31c97d03d49035d55367aae556cb
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-28 13:23:08 -0500

    Se termina de acomodar los component a las vistas

--- fronted/src/views/SobreNosotros.vue
+++ fronted/src/views/SobreNosotros.vue
@@ -1,26 +1,28 @@
+<script setup>
+import FooterVue from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+
+</script>
+
 <template>
   <div>
     <Header />
-    <!-- <div class="container">
-      <h2>¡Visita los lugares más fascinantes del Perú!</h2>
-      <Carrusel />
-    </div> -->
+
+    <!-- Aqui poner tgextoe imagenes referenciales -->
     <div class="container">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, magnam. Ea itaque atque deleniti odit nemo amet est laboriosam ipsam officia fuga, quas veniam ad! Eveniet saepe architecto optio minus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, magnam. Ea itaque atque deleniti odit nemo amet est
+      laboriosam ipsam officia fuga, quas veniam ad! Eveniet saepe architecto optio minus.
       lore
     </div>
   </div>
+
+  <FooterVue />
 </template>
 
-<script setup>
-import Header from '../components/Header.vue';
-import Carrusel from '../components/Carrusel.vue';
-import Valores from '../components/Valores.vue';
-</script>
 
 <style scoped>
-  .container {
-    margin: 0 auto;
-    font-size: 30px;
-  }
+.container {
+  margin: 0 auto;
+  font-size: 30px;
+}
 </style>
